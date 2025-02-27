4d860c4c5fec5279f13884ac5adf653d22336f3d
Vlad Mikhnenok <vlad.mikhnenok.95@gmail.com>
2025-02-27 20:38:49 +0300

    Добавлен MeetingDialog

---
+++ androidApp/src/main/java/ml/vladmikh/projects/find_timer/android/ui/FindMeetingTimeScreen.kt
@@ -0,0 +1,11 @@
+package ml.vladmikh.projects.find_timer.android.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FindMeetingTimeScreen(
+    timeZoneStrings: List<String>,
+
+) {
+
+}
